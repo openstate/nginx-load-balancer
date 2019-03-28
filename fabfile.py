09ffcd373b29bf385aed8807e18a8e18baf4e579
Sicco van Sas <siccovansas@users.noreply.github.com>
2019-03-28 16:17:11 +0100

    Update instructions and fab for deploying a new nginx.conf

--- fabfile.py
+++ fabfile.py
@@ -5,12 +5,22 @@ env.hosts = ["Beryllium", "Carbon", "Nitrogen", "Oxygen"]
 
 
 def deploy_and_reload():
+    "Runs 'git pull' and reloads Nginx"
     with cd('/home/projects/nginx-load-balancer/docker'):
         run('git pull git@github.com:openstate/nginx-load-balancer.git')
         sudo('./reload.sh')
 
 
+def deploy_and_restart():
+    "Runs 'git pull', restarts docker-compose (required if you change nginx.conf) and reloads Nginx"
+    with cd('/home/projects/nginx-load-balancer/docker'):
+        run('git pull git@github.com:openstate/nginx-load-balancer.git')
+        sudo('docker-compose restart')
+        sudo('./reload.sh')
+
+
 def deploy_and_up():
+    "Runs 'git pull', docker-compose up -d (required if you update Nginx/Certbot version) and reloads Nginx"
     with cd('/home/projects/nginx-load-balancer/docker'):
         run('git pull git@github.com:openstate/nginx-load-balancer.git')
         sudo('docker-compose up -d')

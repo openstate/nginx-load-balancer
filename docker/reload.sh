4771cdadf1c5de5f6be71a046b387253d5888e4b
Sicco van Sas <siccovansas@users.noreply.github.com>
2025-04-17 11:58:18 +0200

    Use new docker container name format

--- docker/reload.sh
+++ docker/reload.sh
@@ -2,9 +2,9 @@
 
 # Reload conf/nginx.conf
 echo "*** Reload configuration"
-sudo docker exec docker_c-nginx-load-balancer_1 nginx -t
+sudo docker exec docker-c-nginx-load-balancer-1 nginx -t
 if [ $? -eq 0 ]; then
-  sudo docker exec docker_c-nginx-load-balancer_1 nginx -s reload
+  sudo docker exec docker-c-nginx-load-balancer-1 nginx -s reload
 else
   printf "\nDID NOT RELOAD NGINX CONFIG: NGINX CONFIG GAVE AN ERROR!\n"
 fi
